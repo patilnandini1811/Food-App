26b0b7cc32c579187faada0494817126806aa264
Nandini <patilnandini1811@gmail.com>
2023-04-11 21:14:29 +0200

    Order Items

--- src/components/Cart/card.js
+++ src/components/Cart/card.js
@@ -3,13 +3,26 @@ import {useContext} from 'react';
 import classes from './Card.module.css';
 import Modal from './Modal';
 import CartContext from '../store/cart-context';
+import CartItem from './CartItmes';
 export default function Card(props) {
   const cartCtx = useContext(CartContext);
   const totalAmount= `$ ${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems=cartCtx.items.length >0;
+  const cartItemRemoveHandler=(id)=>{
+
+  }
+  const cartItemAddHandler=(item)=>{
+
+  }
   const cardItems=(<ul className={classes['card-items']}>
   {cartCtx.items.map((item)=>(
-    <li>{item.name}</li>
+    <CartItem key={item.id}
+    name={item.name}
+    amount={item.amount}
+    price={item.price}
+    onRemove={cartItemRemoveHandler.bind(null,item.id)}
+    onAdd={cartItemAddHandler.bind(null,item.id)}
+    />
   ))}
   </ul>
   );
